66a9aa52265684ce68076c15527bf486c6e07aa5
Michel Blanc <mblanc@erasme.org>
2011-11-12 19:15:14 +0100

    added code for image transform

--- 1_lemuriens_neopop/watchconvert/watchconvert.rb
+++ 1_lemuriens_neopop/watchconvert/watchconvert.rb
@@ -1,18 +1,38 @@
 #!/usr/bin/env ruby
 
-require "RMagick"
+require 'RMagick'
+require 'redis'
 
-directory = ARGV[0];
+upload_directory = '/home/user/photos/upload/';
+chair_directories = '/home/user/photos/chaises/'
+image_destination = '/home/user/museomix2011/1_lemuriens_neopop/webglue/public/images/'
 
-# Loop getting 
-image = Magick::Image.read(ARGV[0]).first
+excluded_colors = [ '#B3B3B3' ]
 
+fuzz = ARGV[0] || 8000
 
-Dir.entries(".")
+redis = Redis.new
 
+# Loop getting posted images 
+Dir.entries(upload_directory). do |file|
+  # get oldest redis entry for id_auth and id_chair
+  id_auth = redis.lpop("lemuriens.id_auth")
+  id_chair = redis.lpop("lemuriens.id_chair")
+  
+  # then generate image
+  generate_final(file, id_auth, id_chair)
+  file.delete
+end
 
-image.fuzz = 8000
-#image = image.transparent('#FEE2B0')
-image = image.transparent('#C6C7B2')
-
-image.write("photo.jpg")
+def generate_final(person_image, id_auth, id_chair)
+  image = Magick::Image.read(file)
+  image.fuzz = 8000
+  
+  image.rotate!(-90)
+  
+  excluded_colors.each do |c|
+    image = image.transparent(c)
+  end
+  
+  image.write("#{}/#{id_auth}.jpg") 
+}

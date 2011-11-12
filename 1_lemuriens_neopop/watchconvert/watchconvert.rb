910af11432f77ea58815ce6ddffb2cdac0a8c54d
user <user@erasme.org>
2011-11-12 23:02:05 +0100

    added yaml config file

--- 1_lemuriens_neopop/watchconvert/watchconvert.rb
+++ 1_lemuriens_neopop/watchconvert/watchconvert.rb
@@ -1,37 +1,24 @@
 #!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
 
 require 'RMagick'
 require 'redis'
+require 'yaml'
 
 require 'pp'
 
-$upload_directory = '/home/user/photos/upload/';
-$chair_directories = '/home/user/photos/chaises/'
-$image_destination = '/home/user/museomix2011/1_lemuriens_neopop/webglue/public/images/'
+$config = YAML.load_file("config.yaml")
 
-$excluded_colors = [ # serie gris clair
-                     '#979797', 
-		     '#959998', 
-                     '#797979',
-                     '#B6B8B7', # gris clair
-                     '#8D8D8F', # gris clair
-                     '#A0A0A0', # gris clair
-                     '#AFAFAF', # gris souris
-                     '#89817F', # gris foncé
-                     '#783345', # rose foncé
-                     '#9D1B49', # rose clair
-                     '#94153E', # rose foncé
-		   ]
+pp $config
 
-$debug = true;
 
-$fuzz = ARGV[0] || 2300
+$config['fuzz'] ||= 2300
 
 redis = Redis.new
 
 def generate_final(person_image, id_auth, id_chair)
   pimage = Magick::Image.read("#{person_image}").first
-  cimage = Magick::Image.read("#{$chair_directories}/#{id_chair}.jpg").first
+  cimage = Magick::Image.read("#{$config["paths"]["chair_directories"]}/#{id_chair}.jpg").first
 
   if pimage.nil?
     puts "Ooops, image is nil !"
@@ -45,25 +32,25 @@ def generate_final(person_image, id_auth, id_chair)
 
   pimage.rotate!(-90)
   
-  pimage.fuzz = $fuzz  
-  $excluded_colors.each do |c|
-    puts "Removing color #{c}" if $debug
+  pimage.fuzz = $config['fuzz']  
+  $config['colors']['excluded_colors'].each do |c|
+    puts "Removing color #{c}" if $config['debug']
     pimage = pimage.transparent(c)
   end
  
   dimage = cimage.composite(pimage,Magick::NorthGravity,25,-100, Magick::OverCompositeOp)
  
-  dimage.write("#{$image_destination}/#{id_auth}.jpg") 
-  puts "Wrote #{$image_destination}/#{id_auth}.jpg" if $debug
+  dimage.write("#{$config["paths"]["image_destination"]}/#{id_auth}.jpg") 
+  puts "Wrote #{$config["paths"]["image_destination"]}/#{id_auth}.jpg" if $config['debug']
 end
 
 # Loop getting posted images 
-Dir.entries($upload_directory).each do |file|
-  full_path = "#{$upload_directory}/#{file}"
+Dir.entries($config["paths"]["upload_directory"]).each do |file|
+  full_path = "#{$config["paths"]["upload_directory"]}/#{file}"
   
   next if file == '.' or file == '..'
   
-  puts "Found new image #{full_path}" if $debug
+  puts "Found new image #{full_path}" if $config['debug']
 
   # get oldest redis entry for id_auth and id_chair
   id_auth = redis.lpop("lemuriens.id_auth")
@@ -73,7 +60,7 @@ Dir.entries($upload_directory).each do |file|
     puts "Omg, no ids in REDIS !"
     next
   else
-    puts "Found id_auth/id_chair #{id_auth}/#{id_chair}" if $debug
+    puts "Found id_auth/id_chair #{id_auth}/#{id_chair}" if $config['debug']
   end
 
   # then generate image
